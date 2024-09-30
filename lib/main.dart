e681afcfbe6e0d5551cf8901023fa7592512608e
klaw321 <kushalpokharel234@gmail.com>
2024-09-30 18:43:28 +0000

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; // Firebase Firestore
-import 'package:firebase_core/firebase_core.dart'; // Firebase Core
-import 'package:firebase_auth/firebase_auth.dart'; // Firebase Authentication
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter-twitter',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: AuthScreen(), // Authentication screen
-    );
-  }
-}
-
-// Authentication Screen
 class AuthScreen extends StatelessWidget {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
@@ -33,14 +8,16 @@ class AuthScreen extends StatelessWidget {
       appBar: AppBar(title: Text('Login')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-      body: Center(
-        child: Container(
-            width: 100,
-            height: 100,
-            child: Image.asset('assets/images/kamal.jpg'))
-           ), 
         child: Column(
           children: [
+            // Center widget to display the logo
+            Center(
+              child: Container(
+                width: 100,
+                height: 100,
+                child: Image.asset('assets/images/kamal.jpg'),
+              ),
+            ),
             TextField(
               controller: _emailController,
               decoration: InputDecoration(labelText: 'Email'),
@@ -71,79 +48,3 @@ class AuthScreen extends StatelessWidget {
   }
 }
 
-// Product List Screen
-class ProductListScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Products')),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('products').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) return CircularProgressIndicator();
-          final products = snapshot.data!.docs;
-
-          return ListView.builder(
-            itemCount: products.length,
-            itemBuilder: (context, index) {
-              var product = products[index];
-              return ListTile(
-                title: Text(product['name']),
-                subtitle: Text('\$${product['price']}'),
-                trailing: ElevatedButton(
-                  child: Text('Add to Cart'),
-                  onPressed: () {
-                    FirebaseFirestore.instance.collection('cart').add({
-                      'name': product['name'],
-                      'price': product['price'],
-                    });
-                  },
-                ),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
-}
-
-// Admin Panel for Adding Products
-class AdminPanel extends StatelessWidget {
-  final _nameController = TextEditingController();
-  final _priceController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Admin Panel')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(labelText: 'Product Name'),
-            ),
-            TextField(
-              controller: _priceController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Price'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                FirebaseFirestore.instance.collection('products').add({
-                  'name': _nameController.text,
-                  'price': double.parse(_priceController.text),
-                });
-                _nameController.clear();
-                _priceController.clear();
-              },
-              child: Text('Add Product'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
